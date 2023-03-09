c378b9518e8dd765d3c142b12cfedef0523aa766
Thomas Eng <thomaseng@Thomass-MacBook-Air.local>
2023-03-09 11:21:35 -0500

    we got the schema working

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ module.exports = {
   target: 'web',
   //dev server settings
   devServer: {
+    host: 'localhost',
+    port: 8080,
     //port app will run on
     //directory webpack will use to server static files
     static: {
@@ -29,6 +31,12 @@ module.exports = {
     hot: true,
     //auto update the app as changes occur
     liveReload: true,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000/',
+        secure: false
+      }
+    }
   },
   //specific rules about how webpack will handle diff files
   module: {
@@ -69,3 +77,74 @@ module.exports = {
     })
   ],
 }
+
+//junaids webpack
+// const webpack = require('webpack');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// module.exports = {
+//     entry: [
+//       // entry point of our app
+//       './client/index.js',
+//     ],
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       publicPath: '/',
+//       filename: 'bundle.js',
+//     },
+//     devtool: 'eval-source-map',
+//     mode: 'development',
+//     devServer: {
+//       host: 'localhost',
+//       // enable HMR on the devServer
+//       hot: true,
+//       // fallback to root for other urls
+//       historyApiFallback: {
+//         index: './dist/index.html'
+//       },
+//       static: {
+//         // match the output path
+//         directory: path.resolve(__dirname, 'dist'),
+//         // match the output 'publicPath'
+//         publicPath: '/dist/',
+//       },
+//       headers: { 'Access-Control-Allow-Origin': '*' },
+//       /**
+//        * proxy is required in order to make api calls to
+//        * express server while using hot-reload webpack server
+//        * routes api fetch requests from localhost:8080/api/* (webpack dev server)
+//        * to localhost:3000/api/* (where our Express server is running)
+//        */
+//       proxy: {
+//         '/database': {
+//           target: 'http://localhost:3000/',
+//           secure: false,
+//         },
+//       },
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /.(js|jsx)$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'babel-loader',
+//           },
+//         },
+//         {
+//           test: /.(css|scss)$/,
+//           exclude: /node_modules/,
+//           use: ['style-loader', 'css-loader'],
+//         }
+//       ],
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './client/index.html',
+//       }),
+//     ],
+//     resolve: {
+//       // Enable importing JS / JSX files without specifying their extension
+//       extensions: ['.js', '.jsx'],
+//     },
+//   };
